35370a8cffdb96596f5d351d8dc4eb770f0e66e5
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-27 04:08:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,33 +1,28 @@
-class Artist
+class Artist 
 
-  attr_accessor :name, :artist, :genre
-
-  ALL=[]
+  attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
-    ALL << self
-  end
-
-  def self.all
-    ALL
-  end
-
-  def new_song(name, genre)
-    song = Song.new(name, self, genre)
-    self.songs.last
+    @@all << self
   end
 
   def songs
-    Song.all.select do |song|
-      song.artist == self
-    end
+    Song.all.select {|song| song.artist == self}
   end
 
   def genres
-    Song.all.map do |song|
-      song.genre
-    end
+    songs.map {|song| song.genre}
   end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(song, genre)
+    Song.new(name, self, genre)
+  end
+
 
 end
