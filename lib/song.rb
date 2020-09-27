273631cb0bfebdeee4893a365d252c1af8c63865
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-27 04:09:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,16 @@
-class Song
+class Song 
 
   attr_accessor :name, :artist, :genre
+  @@all = []
 
-  ALL = []
-
-  def initialize(name="", artist="", genre="")
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    ALL << self
+    @@all << self
   end
 
   def self.all
-    ALL
+    @@all
   end
-
-  def artist
-    @artist
-  end
-
 end
