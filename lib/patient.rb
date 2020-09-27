8b939803f9ab3ce949a0280b76528cf99bfd7d57
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-27 04:33:50 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -11,6 +11,10 @@ class Patient
   def self.all
     @@all
   end
+  
+  def new_appointment(doctor, date)
+    Appointment.new(self, date, doctor)
+  end
 
   def appointments
     Appointment.all.select do |appointment|
@@ -22,9 +26,5 @@ class Patient
     Appointment.all.map do |appointment|
       appointment.doctor
     end
-
-  def new_appointment(doctor, date)
-    Appointment.new(self, date, doctor)
-  end
 end
 end
